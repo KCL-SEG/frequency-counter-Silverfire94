85cbb6e807d55443a99fc97267388fbc136bebce
Silverfire94 <markEmmanuel@yahoo.com>
2022-09-28 19:33:35 +0100

    This function should return a count of the different values

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,13 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
+def frequencies(*args):
     frequencies = {}
+    for item in args:
+        item_ = str(item)
+        if item_ in frequencies.keys():
+            frequencies[item_] += 1
+        else:
+            frequencies[item_] = 1
     # Your code goes here
     return frequencies
